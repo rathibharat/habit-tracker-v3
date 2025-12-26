3cbd67d002c1ec172469ca9b57ba7b8130581286
rathibharat <inbox2bharatrathi@gmail.com>
2025-12-25 17:43:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -296,6 +296,7 @@ def secure_headers(response):
     return response
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))  # Render sets the PORT env var
     app.run(host="0.0.0.0", port=port, debug=True)
+
 

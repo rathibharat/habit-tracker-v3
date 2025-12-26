25069595d41638e757171787c94518c0e701c773
rathibharat <inbox2bharatrathi@gmail.com>
2025-12-26 00:56:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -258,6 +258,7 @@ def export():
         writer.writerow([c.habit.name, c.day, c.month, c.year, c.done, c.reason])
     return output
 
-if __name__=="__main__":
-    db.create_all()
-    app.run(debug=True)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=5000)
